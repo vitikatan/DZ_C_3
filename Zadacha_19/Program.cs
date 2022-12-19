d4dc3c11d6c526fac9690be95b450330cf1c203c
Yana <wyemelt@gmail.com>
2022-12-19 19:17:24 +0300

    Zadacha_19 done

---
+++ Zadacha_19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear ();
+Console.Write("Введите пятизначное число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+string str = number + string.Empty;
+
+if (str[0] == str[4] && str[1] == str[3])
+{
+    Console.Write("Это число палиндром");
+}
+else 
+{
+    Console.Write("Это число не палиндром");
+}
