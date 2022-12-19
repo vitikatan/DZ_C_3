9e0aacf07819feac39568192cb657cffd9326efd
Yana <wyemelt@gmail.com>
2022-12-19 20:49:10 +0300

    Zadacha_23 done

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear ();
+Console.WriteLine("Введите число:");
+double number = double.Parse(Console.ReadLine());
+double max = 1;
+double i = 1;
+double stepen =3;
+
+while (i <= number)
+{
+    max = Math.Pow(i, stepen);
+    i = i + 1;
+    Console.WriteLine(max);
+}
