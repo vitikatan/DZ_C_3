94270b12adc0d311cfe0b10a3869ba802924f5da
Yana <wyemelt@gmail.com>
2022-12-19 20:06:28 +0300

    Zadacha_21 done

---
+++ Zadacha_21/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear ();
+Console.WriteLine("Введите координаты первой точки:");
+
+Console.WriteLine("Координаты x1:");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Координаты y1:");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Координаты z1:");
+double z1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки:");
+
+Console.WriteLine("Координаты x2:");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Координаты y2:");
+double y2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Координаты z2:");
+double z2 = double.Parse(Console.ReadLine());
+
+double stepen = 2;
+double S = Math.Sqrt(Math.Pow((x1-x2),stepen) + Math.Pow((y1-y2),stepen) + Math.Pow((z1-z2),stepen));
+Console.WriteLine($"Расстояние между этими точками равно:{S}");
